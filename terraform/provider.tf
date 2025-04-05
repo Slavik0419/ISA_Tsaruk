d4ca2ebfeb2b318b1ca9f620ed44556bc2243a0e
Slavik0419 <115094055+Slavik0419@users.noreply.github.com>
2025-04-05 21:32:15 +0300

    Update provider.tf

---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region  =   "us-east-1" # обраний регіон
+    access_key  =   "AKIAYUKBFSUC5UJRCIH7" 
+    secret_key  =   "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" # секретний ключ
+}
